5b37eb6da61777a5ff3d82e7b60a41a230622505
Marcus Fields <fieldsmu@gmail.com>
2018-05-24 11:55:26 -0400

    added new user database

--- prs-sequel.sql
+++ prs-sequel.sql
@@ -1,11 +1,14 @@
-create table User (
+use master;
+drop database if exists [User];
+create database [User];
+create table [User] (
 	Id int not null identity(1, 1) primary key,
 	Username nvarchar(30) not null unique, 
 	Password nvarchar(30) not null,
 	Firstname nvarchar(30) not null,
 	Lastname nvarchar(30) not null,
 	Phone nvarchar(12) not null,
-	Email nvarchar(255) not null unique,
+	Email nvarchar(255) not null,
 	IsReviewer bit not null,
 	IsAdmin bit not null,
 	Active bit not null
